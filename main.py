d6beb631e02ca2f4fc7ef0676732adb78e909dac
Ritesh Naik <36783070+riteshnaik77@users.noreply.github.com>
2023-09-29 20:46:57 +0530

    Update main.py
    Commenting back the app.run('0.0.0.0')

--- main.py
+++ main.py
@@ -256,5 +256,5 @@ def income_result():
 
 
 if __name__ == "__main__":
-        # app.run(debug=True)
-          app.run(host='0.0.0.0')  
+        app.run(debug=True)
+          # app.run(host='0.0.0.0')  
